86ce24d0ca847e2ab2d023a1fd2d2ad7a92086b9
JeongWonYoung <nicednjsdud@naver.com>
2022-07-28 23:12:33 +0900

    feat logo update about android 12 png
    feat logo next to title

--- Android_workspace/BOB2/app/src/main/java/kr/co/bob2/MainActivity.java
+++ Android_workspace/BOB2/app/src/main/java/kr/co/bob2/MainActivity.java
@@ -3,12 +3,28 @@ package kr.co.bob2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        // activity_main.xml 파일에서 만든 객체에 접근
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
