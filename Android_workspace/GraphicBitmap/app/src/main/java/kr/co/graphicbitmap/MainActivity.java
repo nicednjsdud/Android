a4b7df20dd41bccda50bf13071af1a81efb06dc3
JeongWonYoung <nicednjsdud@naver.com>
2022-08-04 13:02:55 +0900

    feat bitmap method
    feat matrix method

--- Android_workspace/GraphicBitmap/app/src/main/java/kr/co/graphicbitmap/MainActivity.java
+++ Android_workspace/GraphicBitmap/app/src/main/java/kr/co/graphicbitmap/MainActivity.java
@@ -3,13 +3,16 @@ package kr.co.graphicbitmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 
-class MyView extends View{
+class MyView extends View {
     public MyView(Context context) {
         super(context);
     }
@@ -17,7 +20,19 @@ class MyView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        
+
+        // 상하 반전 변환 행렬
+        Matrix matrix = new Matrix();
+        matrix.preScale(1, -1);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.one);
+        Bitmap mBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                                             bitmap.getHeight(), matrix, false);
+
+        Bitmap mBitmap2 = Bitmap.createScaledBitmap(bitmap,100,100,false);
+        canvas.drawBitmap(mBitmap,0,0,null);
+        canvas.drawBitmap(mBitmap2,300,1300,null);
+
     }
 }
 
