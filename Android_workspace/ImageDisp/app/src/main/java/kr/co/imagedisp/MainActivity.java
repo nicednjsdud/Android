f42ff9ff9a6277cd7c586d5aa42f47f17e0fa03e
JeongWonYoung <nicednjsdud@naver.com>
2022-08-04 12:45:46 +0900

    Build project image path
    to study bitmap

--- Android_workspace/ImageDisp/app/src/main/java/kr/co/imagedisp/MainActivity.java
+++ Android_workspace/ImageDisp/app/src/main/java/kr/co/imagedisp/MainActivity.java
@@ -1,12 +1,13 @@
-package kr.co.graphicpath;
+package kr.co.imagedisp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,27 +19,12 @@ class MyView extends View{
 
     @Override
     protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
         Paint paint = new Paint();
-        Path path = new Path();
-
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10);
-
-        // 패스의 시작점 지정
-        path.moveTo(20,400);
-
-        // 패스에 직선을 추가함
-        path.lineTo(300,800);
-
-        // 패스에 곡선을 추가함
-        path.cubicTo(450,120,600,1200,900,800);
-
-        paint.setColor(Color.BLUE);
-        canvas.drawPath(path,paint);
-
-        paint.setTextSize(200);
-        canvas.drawTextOnPath("This is bob!",path,0,0,paint);
+        // 이미지 데이터를 포함하는 앱 리소스 객체 , 이미지 리소스 식별자
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.one);
+        
+        // 화면에 그려질 비트맵, 이미지가 그려질 위치
+        canvas.drawBitmap(bitmap,0,0,paint);
     }
 }
 
@@ -47,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MyView(this));
+        MyView myView = new MyView(this);
+        setContentView(myView);
     }
 }
