6ff69fbaf33eb2819e3f1eb828d9757306774598
JeongWonYoung <nicednjsdud@naver.com>
2022-08-04 09:52:38 +0900

    feat mainactivity function
    paint, canvas impl

--- Android_workspace/Grapic/app/src/main/java/kr/co/grapic/MainActivity.java
+++ Android_workspace/Grapic/app/src/main/java/kr/co/grapic/MainActivity.java
@@ -2,13 +2,43 @@ package kr.co.grapic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+
+class MyView extends View{
+    public MyView(Context context) {
+        super(context);
+        setBackgroundColor(Color.BLUE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(20);
+        // 선
+        canvas.drawLine(100,100,700,100,paint);
+        // 사각형
+        canvas.drawRect(100,300,700,700,paint);
+        // 원
+        canvas.drawCircle(300,1200,200,paint);
+        paint.setTextSize(80);
+        // 텍스트
+        canvas.drawText("Hello BOB!",100,900,paint);
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MyView myView = new MyView(this);
+        setContentView(myView);
     }
 }
