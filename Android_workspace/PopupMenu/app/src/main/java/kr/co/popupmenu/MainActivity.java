0efc2536b2371a06c612dc894604faf093aecc8a
JeongWonYoung <nicednjsdud@naver.com>
2022-08-05 12:50:39 +0900

    feat popmenu method

--- Android_workspace/PopupMenu/app/src/main/java/kr/co/popupmenu/MainActivity.java
+++ Android_workspace/PopupMenu/app/src/main/java/kr/co/popupmenu/MainActivity.java
@@ -3,6 +3,10 @@ package kr.co.popupmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View button){
+        PopupMenu popupMenu = new PopupMenu(this,button);
+        popupMenu.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(getApplicationContext(),"클릭된 팝업메뉴 : "+menuItem.getTitle(),
+                                                            Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        popupMenu.show();
     }
 }
