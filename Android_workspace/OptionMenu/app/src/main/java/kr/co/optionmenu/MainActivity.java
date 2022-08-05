151f25cedd3a27cb827ef61ece029c00ac3c35e9
JeongWonYoung <nicednjsdud@naver.com>
2022-08-05 11:47:33 +0900

    feat mymenu bar event
    event

--- Android_workspace/OptionMenu/app/src/main/java/kr/co/optionmenu/MainActivity.java
+++ Android_workspace/OptionMenu/app/src/main/java/kr/co/optionmenu/MainActivity.java
@@ -1,14 +1,48 @@
 package kr.co.optionmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = findViewById(R.id.layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mymenu, menu);  // 매뉴 리소스 팽창
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.blue:
+                view.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.green:
+                view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.red:
+                view.setBackgroundColor(Color.RED);
+                return true;
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
