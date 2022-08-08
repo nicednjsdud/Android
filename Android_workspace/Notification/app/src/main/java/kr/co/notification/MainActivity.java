70c4d55efb813142684d4002e1a7439861725827
JeongWonYoung <nicednjsdud@naver.com>
2022-08-08 15:44:26 +0900

    feat notification alert

--- Android_workspace/Notification/app/src/main/java/kr/co/notification/MainActivity.java
+++ Android_workspace/Notification/app/src/main/java/kr/co/notification/MainActivity.java
@@ -1,14 +1,61 @@
 package kr.co.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+    }
+
+    // 1. 알림 채널 생성 하기
+    private void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Channel Description");
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+    }
+
+    // 2. 알림 빌더 생성함.
+    public void sendNotification(View view) {
+        NotificationCompat.Builder notificationBuilder
+                = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        //  4. 알림이 클릭되면 아래 인텐트가 보내짐 (액션 첨부)
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        // 3. 알림 속성 설정
+        notificationBuilder.setSmallIcon(R.drawable.android)
+                .setContentTitle("메일 알림")
+                .setContentText("새로운 메일이 도착했습니다.")
+                .setContentIntent(pendingIntent);
+
+        // 5. 알림 객체 생성하여 보내기
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1,notificationBuilder.build());
+
     }
 }
