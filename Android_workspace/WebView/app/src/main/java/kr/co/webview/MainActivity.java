7981ef93f996dfb9115541a2c0b5b2d9728ce3eb
JeongWonYoung <nicednjsdud@naver.com>
2022-08-08 16:21:36 +0900

    Feat webView method

--- Android_workspace/WebView/app/src/main/java/kr/co/webview/MainActivity.java
+++ Android_workspace/WebView/app/src/main/java/kr/co/webview/MainActivity.java
@@ -3,12 +3,53 @@ package kr.co.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editUrl;
+    Button btnGo,btnBack;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editUrl = findViewById(R.id.edtUrl);
+        btnGo = findViewById(R.id.btnGo);
+        btnBack = findViewById(R.id.btnBack);
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new BobWebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.loadUrl(editUrl.getText().toString());
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.goBack();
+            }
+        });
+    }
+
+    private class BobWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
     }
 }
