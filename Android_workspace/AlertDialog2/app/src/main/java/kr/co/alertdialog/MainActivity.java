f095cac7d9e3099cc736dc582eb58003bade4d4b
JeongWonYoung <nicednjsdud@naver.com>
2022-08-08 12:04:02 +0900

    feat alert dialog xml

--- Android_workspace/AlertDialog2/app/src/main/java/kr/co/alertdialog/MainActivity.java
+++ Android_workspace/AlertDialog2/app/src/main/java/kr/co/alertdialog/MainActivity.java
@@ -1,14 +1,90 @@
 package kr.co.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvName, tvEmail;
+    Button button;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        setTitle("사용자 정보 입력(수정)");
+
+        tvName = findViewById(R.id.tvName);
+        tvEmail = findViewById(R.id.tvEmail);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogView = View.inflate(MainActivity.this, R.layout.dialog, null);
+                // 대화상자 생성 (빌더 객체 생성)
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                // 용도에 따른 설정
+                // 제목 설정
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.ic_menu_allfriends);
+                dlg.setView(dialogView);
+
+                dlgEdtName = dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdtEmail = dialogView.findViewById(R.id.dlgEdt2);
+
+                dlgEdtName.setText(tvName.getText().toString());
+                dlgEdtEmail.setText(tvEmail.getText().toString());
+
+                // 대화상자의 'ok" 버튼 부분 설정
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+
+                // 대화상자의 'Cancel' 버튼 부분 설정
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast toast = new Toast(MainActivity.this);
+
+                        // 토스트가 화면의 임의 위치에 나타내기
+                        Display display =
+                                ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
+                        int xOffset = (int) (Math.random() * display.getWidth());
+                        int yOffset = (int) (Math.random() * display.getHeight());
+
+                        toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
+
+                        toastView = View.inflate(MainActivity.this,R.layout.toast,null);
+                        toastText.setText("취소했습니다!");
+
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+
+                dlg.show();
+            }
+        });
     }
 }
