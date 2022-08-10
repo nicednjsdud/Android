96ea030195adbff64bc01a9c15fb1c39fa0997c4
JeongWonYoung <nicednjsdud@naver.com>
2022-08-10 12:24:11 +0900

    feat spinner event listener

--- Android_workspace/Spinner/app/src/main/java/kr/co/spinner/MainActivity.java
+++ Android_workspace/Spinner/app/src/main/java/kr/co/spinner/MainActivity.java
@@ -3,6 +3,11 @@ package kr.co.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        // 리소스부터 ArrayAdapter를 생성하는 메소드 호출
+        ArrayAdapter<CharSequence> adapter =
+        ArrayAdapter.createFromResource(
+                    this,R.array.planets_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // 스피너와 어댑터 연결
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                Toast.makeText(adapterView.getContext(),
+                        "선택된 행성은 "+adapterView.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
