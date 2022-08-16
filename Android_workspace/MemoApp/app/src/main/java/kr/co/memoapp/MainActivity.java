dca350851d9531df75b8b9d8408186fa9be9d341
JeongWonYoung <nicednjsdud@naver.com>
2022-08-16 14:56:44 +0900

    feat file method

--- Android_workspace/MemoApp/app/src/main/java/kr/co/memoapp/MainActivity.java
+++ Android_workspace/MemoApp/app/src/main/java/kr/co/memoapp/MainActivity.java
@@ -2,13 +2,71 @@ package kr.co.memoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    String FILENAME="";
+    EditText fileName,memo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fileName = findViewById(R.id.fileName);
+        memo = findViewById(R.id.memo);
+
+        Button writeBtn = findViewById(R.id.write);
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FILENAME = fileName.getText().toString();
+                try {
+                    FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+                    fos.write(memo.getText().toString().getBytes());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Button readBtn = findViewById(R.id.read);
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FILENAME = fileName.getText().toString();
+                try {
+                    FileInputStream fis = openFileInput(FILENAME);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    memo.setText(new String(buffer));
+                    fis.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button deleteBtn = findViewById(R.id.delete);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteFile(FILENAME);
+                memo.setText("");
+            }
+        });
     }
 }
