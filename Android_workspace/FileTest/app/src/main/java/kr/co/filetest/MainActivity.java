c4764625603fb232988ead26658587fd1ddef078
JeongWonYoung <nicednjsdud@naver.com>
2022-08-16 13:50:55 +0900

    feat mainActivity method

--- Android_workspace/FileTest/app/src/main/java/kr/co/filetest/MainActivity.java
+++ Android_workspace/FileTest/app/src/main/java/kr/co/filetest/MainActivity.java
@@ -2,13 +2,62 @@ package kr.co.filetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    String FILENAME = "BOBTEST.txt";
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.EditText);
+
+        Button readBtn = findViewById(R.id.read);
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    FileInputStream fis = openFileInput(FILENAME);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    editText.setText(new String(buffer));
+                    fis.close();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        Button writeBtn = findViewById(R.id.write);
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+                    fos.write(editText.getText().toString().getBytes());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
