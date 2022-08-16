11499ba3cd3bdf414658a04227079ffc65a31b51
JeongWonYoung <nicednjsdud@naver.com>
2022-08-16 11:25:26 +0900

    Build project ActivityLifeCycle
    to study life cycle

--- Android_workspace/LifeCycle/app/src/main/java/kr/co/lifecycle/MainActivity.java
+++ Android_workspace/LifeCycle/app/src/main/java/kr/co/lifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package kr.co.lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("LifeCycle","onCreate() 호출");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("LifeCycle","onStart() 호출");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("LifeCycle","onResume() 호출");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("LifeCycle","onPause() 호출");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("LifeCycle","onStop() 호출");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("LifeCycle","onDestroy() 호출");
     }
 }
