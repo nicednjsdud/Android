4e43fa31ded0cc9041a6cc348e694eecf487725e
JeongWonYoung <nicednjsdud@naver.com>
2022-08-16 12:12:39 +0900

    feat mainactivity method

--- Android_workspace/ActivityLifeCycle/app/src/main/java/kr/co/activitylifecycle/MainActivity.java
+++ Android_workspace/ActivityLifeCycle/app/src/main/java/kr/co/activitylifecycle/MainActivity.java
@@ -2,7 +2,12 @@ package kr.co.activitylifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("액티비티 생명주기","onCreate()");
+        Button btnDial = findViewById(R.id.btnDial);
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:010-0000-0000");
+                Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(intent);
+            }
+        });
+        Button btnFinish = findViewById(R.id.btnFinish);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("액티비티 생명주기","onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("액티비티 생명주기","onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("액티비티 생명주기","onDestroy()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("액티비티 생명주기","onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("액티비티 생명주기","onResume()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("액티비티 생명주기","onRestart()");
     }
 }
