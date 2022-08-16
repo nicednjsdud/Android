b69f750a184a37985dca3dee1793804b9ecedb9c
JeongWonYoung <nicednjsdud@naver.com>
2022-08-16 11:12:06 +0900

    feat implicit intent method

--- Android_workspace/ImplicitIntent/app/src/main/java/kr/co/implicitintent/MainActivity.java
+++ Android_workspace/ImplicitIntent/app/src/main/java/kr/co/implicitintent/MainActivity.java
@@ -2,7 +2,10 @@ package kr.co.implicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.web:
+                // 액션이 ACTION_VIEW인 인텐트를 생성함. www.apple.com를 데이터로 설정
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.apple.com"));
+                break;
+            case R.id.call:
+                intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:1544-0000"));
+                break;
+            case R.id.map:
+                intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:37.502072295376, 127.02451465959"));
+                break;
+            case R.id.contact:
+                intent = new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/people/"));
+                break;
+        }
+        if (intent !=null){
+            // 인텐트 시작
+            startActivity(intent);
+        }
     }
 }
