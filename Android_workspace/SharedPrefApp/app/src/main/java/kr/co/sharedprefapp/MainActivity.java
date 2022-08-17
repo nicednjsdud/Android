3c542eee4fee90df2d8fd71b6bc221fcf6938995
JeongWonYoung <nicednjsdud@naver.com>
2022-08-17 09:10:17 +0900

    Build project MyDatabase
    to study db

--- Android_workspace/SharedPrefApp/app/src/main/java/kr/co/sharedprefapp/MainActivity.java
+++ Android_workspace/SharedPrefApp/app/src/main/java/kr/co/sharedprefapp/MainActivity.java
@@ -2,13 +2,41 @@ package kr.co.sharedprefapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "MyPrefs";
+    TextView name;
+    EditText value;
+    String imageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.TextView);
+        value = findViewById(R.id.EditText);
+
+        // 프레퍼런스를 복원함.
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+        imageName = settings.getString("imageName","");
+        value.setText(imageName);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        // 프레퍼런스에 값을 기록하려면 Editor 객체가 필요함.
+        SharedPreferences.Editor editor = settings.edit();
+        imageName = value.getText().toString();
+        editor.putString("imageName",imageName);
+        // 변경을 최종 반영함.
+        editor.commit();
+
     }
 }
